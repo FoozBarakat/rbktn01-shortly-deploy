78abde9aefd6fbb6de466539f505a32c5f55bb70
FoozBarakat <reachfooz@gmail.com>
2019-12-25 14:54:13 +0100

    second deploy

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var app = require('./server-config.js');
 
 var port = 4568;
 
-app.listen(port);
+app.listen(
+  // we might need to edit this.
+  //environment.port||
+  port);
 
 console.log('Server now listening on port ' + port);
